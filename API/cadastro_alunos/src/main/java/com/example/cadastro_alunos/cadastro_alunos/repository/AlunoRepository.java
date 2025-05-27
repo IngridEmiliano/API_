b900f97add63786a66edc5b7d5161238612e6e39
Ingrid <ingridemilianotrab@gmail.com>
2025-05-26 23:19:19 -0300

    AlunoRepository.java

--- API/cadastro_alunos/src/main/java/com/example/cadastro_alunos/cadastro_alunos/repository/AlunoRepository.java
+++ API/cadastro_alunos/src/main/java/com/example/cadastro_alunos/cadastro_alunos/repository/AlunoRepository.java
@@ -1,6 +1,6 @@
 package com.example.cadastro_alunos.cadastro_alunos.repository;
 
-import com.example.cadastro_alunos.cadastro_alunos.repository;
+import com.example.cadastro_alunos.cadastro_alunos.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
