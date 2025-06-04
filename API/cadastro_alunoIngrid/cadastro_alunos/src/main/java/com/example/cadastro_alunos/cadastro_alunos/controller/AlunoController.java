c31a3ead79a3e033dc300aea2bbc98e5ef4510d2
Ingrid <ingridemilianotrab@gmail.com>
2025-06-03 23:08:07 -0300

    AlunoController.java

--- API/cadastro_alunoIngrid/cadastro_alunos/src/main/java/com/example/cadastro_alunos/cadastro_alunos/controller/AlunoController.java
+++ API/cadastro_alunoIngrid/cadastro_alunos/src/main/java/com/example/cadastro_alunos/cadastro_alunos/controller/AlunoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*") //Permite que o front acesse a API
 @RestController
 @RequestMapping("/api/aluno")
 
