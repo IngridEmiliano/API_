489b454b3c39fbb010cb41f4367ac3ee66e13f14
Ingrid <ingridemilianotrab@gmail.com>
2025-06-10 23:54:30 -0300

    Delete API/rh-tech directory

--- API/rh-tech/rh-tech/src/main/java/com/example/rh_tech/Repository/CargosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rh_tech.Repository;
-
-import com.example.rh_tech.Model.CargosModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CargosRepository extends JpaRepository<CargosModel, Long> {
-} 
