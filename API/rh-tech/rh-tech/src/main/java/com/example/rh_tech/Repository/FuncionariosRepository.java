274732827ad99e8e66f28048d2fe953cbbc76f69
Ingrid <ingridemilianotrab@gmail.com>
2025-06-10 23:53:11 -0300

    Delete API/rh-tech directory

--- API/rh-tech/rh-tech/src/main/java/com/example/rh_tech/Repository/FuncionariosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rh_tech.Repository;
-
-import com.example.rh_tech.Model.FuncionariosModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FuncionariosRepository extends JpaRepository<FuncionariosModel, Long> {
-} 
