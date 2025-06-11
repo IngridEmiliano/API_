12a2e4c86e77bd1a6e0a94c4090493ee93b46529
Ingrid <ingridemilianotrab@gmail.com>
2025-06-10 23:53:53 -0300

    Add files via upload

---
+++ API/rh-tech/rh-tech/src/main/java/com/example/rh_tech/Model/CargosModel.java
@@ -0,0 +1,30 @@
+package com.example.rh_tech.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cargos")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class CargosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column
+    private String descricao;
+   
+}
